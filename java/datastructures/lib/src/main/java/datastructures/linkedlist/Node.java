cb0c7cc3d10c110c7db83a8843d092fee309b1e4
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-17 09:55:05 -0500

    Updated Comment on node.java

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/Node.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/Node.java
@@ -3,9 +3,9 @@ package datastructures.linkedlist;
 public class Node
 {
     public int value;
-    public Node next = null;
+    public Node next = null; // Must have this for Line 9 of LinkList.java to work (insertNode.next)
 
-    Node(int value) // This is the constructor
+    Node(int value) // This is the constructor being called in line 8 of LinkList.java(new Node)
     {
         this.value = value;
         this.next = null;
